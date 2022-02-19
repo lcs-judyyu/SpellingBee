e354a0e135a6ece56ffc0a17530f89bdc7caa770
Judy <jyu@lcs.on.ca>
2022-02-18 23:53:10 -0500

    Add text on top of the image

--- SpellingBee/Views/ContentView.swift
+++ SpellingBee/Views/ContentView.swift
@@ -40,29 +40,39 @@ struct ContentView: View {
         
         VStack {
             
-            Image(currentItem.imageName)
-                .resizable()
-                .scaledToFit()
-                .hoverEffect(.lift)
-                .onTapGesture {
-                    
-                    // Create the word to be spoken (an utterance) and set
-                    // characteristics of how the voice will sound
-                    let utterance = AVSpeechUtterance(string: currentItem.word)
-                    
-                    // See a list of available language codes and their corresponding
-                    // voice names and genders here:
-                    // https://www.ikiapps.com/tips/2015/12/30/setting-voice-for-tts-in-ios
-                    utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
-                    
-                    // How fast the utterance will be spoken
-                    utterance.rate = 0.5
-                    
-                    // Synthesize (speak) the utterance
-                    let synthesizer = AVSpeechSynthesizer()
-                    synthesizer.speak(utterance)
-                    
-                }
+            ZStack {
+                Image(currentItem.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .hoverEffect(.lift)
+                    .onTapGesture {
+                        
+                        // Create the word to be spoken (an utterance) and set
+                        // characteristics of how the voice will sound
+                        let utterance = AVSpeechUtterance(string: currentItem.word)
+                        
+                        // See a list of available language codes and their corresponding
+                        // voice names and genders here:
+                        // https://www.ikiapps.com/tips/2015/12/30/setting-voice-for-tts-in-ios
+                        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+                        
+                        // How fast the utterance will be spoken
+                        utterance.rate = 0.5
+                        
+                        // Synthesize (speak) the utterance
+                        let synthesizer = AVSpeechSynthesizer()
+                        synthesizer.speak(utterance)
+                        
+                    }
+                
+                Text("Tap me")
+                    .font(.title2)
+                    .italic()
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 15)
+                    .background(Color.white.opacity(0.6))
+                    .clipShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
+            }
             
             TextField("Enter your answer here",
                       text: $inputGiven)
